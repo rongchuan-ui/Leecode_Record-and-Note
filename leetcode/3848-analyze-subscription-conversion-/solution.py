6cf28baa25b9c388b4a7af93dc3f9e2a52269ab5
Rongchuan Shi <rongchuan@berkeley.edu>
2026-04-02 00:21:21 -0700

    Update solution.py

--- leetcode/3848-analyze-subscription-conversion-/solution.py
+++ leetcode/3848-analyze-subscription-conversion-/solution.py
@@ -29,3 +29,25 @@ def analyze_subscription_conversion(user_activity: pd.DataFrame) -> pd.DataFrame
     
     df=user_activity_free.merge(user_activity_paid,on="user_id",how="inner")
     return df    
+
+# other way
+import pandas as pd
+
+def analyze_subscription_conversion(user_activity: pd.DataFrame) -> pd.DataFrame:
+    df = (
+        user_activity.groupby(["user_id", "activity_type"])["activity_duration"]
+        .mean()
+        .unstack()
+        .reset_index()
+    )
+
+    df = df[
+        df["free_trial"].notna() & df["paid"].notna()
+    ]
+
+    df = df.rename(columns={
+        "free_trial": "trial_avg_duration",
+        "paid": "paid_avg_duration"
+    })
+
+    return df[["user_id", "trial_avg_duration", "paid_avg_duration"]]
