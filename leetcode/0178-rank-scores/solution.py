2f843fe25b2f2831314423f9ba492b4e457a05a5
Rongchuan Shi <rongchuan@berkeley.edu>
2026-04-02 10:07:53 +0000

    [LeetCode Sync] Runtime - 412 ms (5.02%), Memory - 68.1 MB (73.30%)

---
+++ leetcode/0178-rank-scores/solution.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+    j=1
+    scores=scores.sort_values("score",ascending=False).reset_index(drop=True)
+    scores["rank"] = 0  
+    for i in range(0,len(scores["score"])):
+        if i+1 <len(scores["score"]):
+            if scores.loc[i,"score"] == scores.loc[i+1,"score"]:
+                scores.loc[i:i+1,"rank"]=j
+            else:
+                scores.loc[i,"rank"]=j
+                j+=1
+        else:
+            scores.loc[i,"rank"]=j
+    return pd.DataFrame({
+        "score":scores["score"],
+        "rank":scores["rank"]
+    })
+
+# def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
+#     scores = scores.sort_values("score", ascending=False).copy()
+#     scores["rank"] = scores["score"].rank(method="dense", ascending=False).astype(int)
+#     return scores[["score", "rank"]]
+    
