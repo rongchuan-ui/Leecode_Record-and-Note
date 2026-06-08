974340e513643ca9a26947362d76a9db70d01463
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-07 18:13:40 -0700

    Time: 0 ms (100.00%) | Memory: 19.3 MB (33.03%) - LeetSync

---
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.py
@@ -0,0 +1,12 @@
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1+str2 != str2+str1:
+            return ""
+        a= max(len(str1),len(str2))
+        b= min(len(str1),len(str2))
+        def innergcd(a,b):
+            if not b:
+                return a
+            return innergcd(b,a%b)
+        gcd_len = innergcd(a, b)
+        return str1[:gcd_len]
