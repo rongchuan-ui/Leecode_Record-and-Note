b065074f8d8e95aaeb072289cf88563099ae4b9b
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-15 20:12:21 -0700

    Time: 466 ms (72.08%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,8 @@
+-- Write your PostgreSQL query statement below
+select v.customer_id,
+    count(*) As count_no_trans
+from Visits v
+left join Transactions t
+on v.visit_id = t.visit_id
+where transaction_id is null
+group by v.customer_id;
