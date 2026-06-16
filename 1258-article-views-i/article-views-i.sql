9be2a92b41166456993b0290e17235a9b19946d0
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-15 19:12:50 -0700

    Time: 365 ms (41.53%) | Memory: 0B (100.00%) - LeetSync

--- 1258-article-views-i/article-views-i.sql
+++ 1258-article-views-i/article-views-i.sql
@@ -1,14 +1,15 @@
 -- Write your PostgreSQL query statement below
--- select article_id As id
--- from (
---     select article_id
---     from Views
---     where article_id = viewer_id
--- ) author_view v
--- group by v.article_id
--- having count(v.article_id) >=1;
+select v.author_id As id
+from (
+    select author_id
+    from Views
+    where author_id = viewer_id
+) v
+group by v.author_id
+having count(v.author_id) >=1
+order by id ASC;
 
-select distinct author_id As id
-from Views
-where author_id = viewer_id
-order by author_id asc;
+-- select distinct author_id As id
+-- from Views
+-- where author_id = viewer_id
+-- order by author_id asc;
