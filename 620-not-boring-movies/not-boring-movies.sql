ee53c0210118781e002f8395dd74fd53aafa519a
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-15 20:40:40 -0700

    Time: 185 ms (64.15%) | Memory: 0B (100.00%) - LeetSync

--- 620-not-boring-movies/not-boring-movies.sql
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -1,5 +1,10 @@
 -- Write your PostgreSQL query statement below
+-- select id, movie, description, rating
+-- from Cinema
+-- where mod(id,2) !=0 and description != 'boring'
+-- order by rating desc;
+
 select id, movie, description, rating
 from Cinema
-where mod(id,2) !=0 and description != 'boring'
+where id-(id/2)*2 !=0 and description != 'boring'
 order by rating desc;
