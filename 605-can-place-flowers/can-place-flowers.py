9e1a44bcf4aa254f513508ac19d04eebdf936f7d
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-17 16:55:13 -0700

    Time: 11 ms (47.42%) | Memory: 19.8 MB (10.43%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.py
@@ -0,0 +1,76 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        interval_number=[]
+        m=0
+        h=0
+        if flowerbed[0]==1:
+            interval_number.append(0)
+        else:
+            m+=1
+            if len(flowerbed)==1:
+                interval_number.append(m)
+        for i in range(1,len(flowerbed)):
+            if flowerbed[i]==1:
+                interval_number.append(m)
+                interval_number.append(0)
+                m=0
+            elif flowerbed[i] !=1 and i== len(flowerbed)-1:
+                m+=1
+                interval_number.append(m)
+            else:
+                m+=1
+            i+=1
+        # return interval_number
+        if interval_number[0] == 0 and interval_number[-1] == 0:
+            for j in range(len(interval_number)):
+                if interval_number[j]<=2:
+                    h+=0
+                if interval_number[j]>2 and interval_number[j]%2 == 0:
+                    h += (interval_number[j]-1) // 2
+                if interval_number[j]>2 and interval_number[j]%2 != 0:
+                    h+= interval_number[j] // 2
+                j+=1
+        if interval_number[0] != 0 and interval_number[-1] == 0:
+            h+=interval_number[0]//2
+            for j in range(1,len(interval_number)):
+                if interval_number[j]<=2:
+                    h+=0
+                if interval_number[j]>2 and interval_number[j]%2 == 0:
+                    h += (interval_number[j]-1) // 2
+                if interval_number[j]>2 and interval_number[j]%2 != 0:
+                    h+= interval_number[j] // 2
+                j+=1
+        if interval_number[0] == 0 and interval_number[-1] != 0:
+            h+=interval_number[-1]//2
+            for j in range(len(interval_number)-1):
+                if interval_number[j]<=2:
+                    h+=0
+                if interval_number[j]>2 and interval_number[j]%2 == 0:
+                    h += (interval_number[j]-1) // 2
+                if interval_number[j]>2 and interval_number[j]%2 != 0:
+                    h+= interval_number[j] // 2
+                j+=1
+        if interval_number[0] != 0 and interval_number[-1] != 0:
+            if len(interval_number) == 1:
+                if interval_number[-1]%2 == 0:
+                    h= interval_number[-1]//2
+                else:
+                    h = (interval_number[-1]+1)//2
+            else:
+                h+=interval_number[-1]//2
+                h+=interval_number[0]//2
+                for j in range(1,(len(interval_number)-1)):
+                    if interval_number[j]<=2:
+                        h+=0
+                    if interval_number[j]>2 and interval_number[j]%2 == 0:
+                        h += (interval_number[j]-1) // 2
+                    if interval_number[j]>2 and interval_number[j]%2 != 0:
+                        h+= interval_number[j] // 2
+                    j+=1
+        if h>=n:
+            return True
+        else:
+            return False
+
+
+            
