3167f002bc2321ad49e023cd769144d703291aaa
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-17 12:51:51 -0700

    Time: 294 ms (13.38%) | Memory: 0B (100.00%) - LeetSync

---
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -0,0 +1,14 @@
+-- Write your PostgreSQL query statement below
+With total_confirm as (
+    select user_id,
+           count(*) as total_request,
+           sum(case when action='confirmed' then 1 else 0 end) as total_confirmed_request
+    from Confirmations
+    group by user_id
+)
+select s.user_id,
+        case when t.user_id is null then 0 
+             else round(t.total_confirmed_request::numeric/t.total_request::numeric,2) end as confirmation_rate
+from Signups s
+left join total_confirm t
+on s.user_id=t.user_id;
