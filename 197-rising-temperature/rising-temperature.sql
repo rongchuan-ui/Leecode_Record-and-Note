a65cadcd37116a0556ce0ed2edab28b8f87241d4
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-18 15:31:38 -0700

    Time: 212 ms (88.40%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,12 +1,35 @@
 -- Write your PostgreSQL query statement below
-SELECT lag.id AS Id
-FROM (
-    SELECT id, 
-           temperature,
-           recordDate,
-           LAG(temperature, 1) OVER (ORDER BY recordDate) AS lag_temperature_1,
-           LAG(recordDate, 1) OVER (ORDER BY recordDate) AS lag_date_1
-    FROM Weather
-) lag
-WHERE lag.lag_temperature_1 < lag.temperature
-  AND (lag.recordDate - lag.lag_date_1) = 1;
+-- SELECT lag.id AS Id
+-- FROM (
+--     SELECT id, 
+--            temperature,
+--            recordDate,
+--            LAG(temperature, 1) OVER (ORDER BY recordDate) AS lag_temperature_1,
+--            LAG(recordDate, 1) OVER (ORDER BY recordDate) AS lag_date_1
+--     FROM Weather
+-- ) lag
+-- WHERE lag.lag_temperature_1 < lag.temperature
+--   AND (lag.recordDate - lag.lag_date_1) = 1;
+
+-- with lag as (
+--     SELECT id, 
+--            temperature,
+--            recordDate,
+--            LAG(temperature, 1) OVER (ORDER BY recordDate) AS lag_temperature_1,
+--            LAG(recordDate, 1) OVER (ORDER BY recordDate) AS lag_date_1
+--     FROM Weather
+-- )
+-- select id
+-- from lag
+-- where lag_temperature_1>temperature and lag_date_1+1=recordDate;
+
+SELECT 
+    w1.id
+FROM 
+    Weather w1
+JOIN 
+    Weather w2
+ON 
+    w1.recordDate= w2.recordDate+1
+and 
+    w1.temperature > w2.temperature;
