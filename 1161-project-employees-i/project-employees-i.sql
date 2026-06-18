cb75b1ec7cc052a6a1b4aa6b27320dfef640e984
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-18 15:54:52 -0700

    Time: 465 ms (44.76%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,6 +1,6 @@
 -- Write your PostgreSQL query statement below
 select p.project_id,
-    round(sum(e.experience_years)::Numeric / count(distinct e.employee_id)::Numeric,2) as average_years
+    round(avg(e.experience_years),2) as average_years
 from Project p
 inner join Employee e
 on p.employee_id=e.employee_id
