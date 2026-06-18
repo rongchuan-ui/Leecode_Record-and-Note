326ee138828a92cd5c3e14d98798e3888974136b
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-18 16:50:01 -0700

    Time: 362 ms (97.24%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,12 +1,25 @@
 -- Write your PostgreSQL query statement below
-with first_order_date as (
-    select customer_id,
-            min(order_date) as first_order_date
-    from Delivery
-    group by customer_id
+-- with first_order_date as (
+--     select customer_id,
+--             min(order_date) as first_order_date
+--     from Delivery
+--     group by customer_id
+-- )
+-- select 
+--     round(count(f.customer_id) filter (where d.order_date=d.customer_pref_delivery_date)::numeric / count(f.customer_id)::numeric*100,2) as immediate_percentage
+-- from Delivery d
+-- inner join first_order_date f
+-- on f.first_order_date=d.order_date and d.customer_id=f.customer_id
+
+WITH ranked_orders AS (
+    SELECT *,
+           ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date) AS rn
+    FROM Delivery
 )
-select 
-    round(count(f.customer_id) filter (where d.order_date=d.customer_pref_delivery_date)::numeric / count(f.customer_id)::numeric*100,2) as immediate_percentage
-from Delivery d
-inner join first_order_date f
-on f.first_order_date=d.order_date and d.customer_id=f.customer_id
+SELECT 
+    ROUND(
+        AVG((order_date = customer_pref_delivery_date)::INT) * 100, 
+        2
+    ) AS immediate_percentage
+FROM ranked_orders
+WHERE rn = 1;
