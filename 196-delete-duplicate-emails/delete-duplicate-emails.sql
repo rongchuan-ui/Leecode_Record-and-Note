bb4d5a6850e7cb18d4adfa22080cd97d1fc0aefe
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-18 17:07:41 -0700

    Time: 228 ms (80.22%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -1,5 +1,12 @@
 -- Write your PostgreSQL query statement below
-Delete from Person p1
-using Person p2
-where p1.email=p2. email
-and p1.id>p2.id;
+-- Delete from Person p1
+-- using Person p2
+-- where p1.email=p2. email
+-- and p1.id>p2.id;
+
+DELETE FROM Person
+WHERE id NOT IN (
+    SELECT MIN(id)
+    FROM Person
+    GROUP BY email
+);
