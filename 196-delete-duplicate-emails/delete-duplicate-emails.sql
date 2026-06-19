78fb7c275c2cc2caec1ed042ef9ff6e2a75c7fda
Rongchuan Shi <rongchuan@berkeley.edu>
2026-06-18 17:08:14 -0700

    Time: 261 ms (25.53%) | Memory: 0B (100.00%) - LeetSync

--- 196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ 196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -4,9 +4,19 @@
 -- where p1.email=p2. email
 -- and p1.id>p2.id;
 
+-- DELETE FROM Person
+-- WHERE id NOT IN (
+--     SELECT MIN(id)
+--     FROM Person
+--     GROUP BY email
+-- );
+
 DELETE FROM Person
-WHERE id NOT IN (
-    SELECT MIN(id)
-    FROM Person
-    GROUP BY email
+WHERE id IN (
+    SELECT id 
+    FROM (
+        SELECT id, ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) AS rn
+        FROM Person
+    ) t
+    WHERE rn > 1
 );
